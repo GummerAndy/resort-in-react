bab2e81b30b491c0b5cdfed2e6d780b83704b7c2
Andy Gummer <gummer.andy@gmail.com>
2020-02-25 13:27:45 +0000

    inline svg added

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ import Error from "./pages/Error";
 
 import { Route, Switch } from "react-router-dom";
 
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <>
+      <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />
