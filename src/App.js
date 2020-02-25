228de5bc820ef584eef1634b5fd0649064acbb11
Andy Gummer <gummer.andy@gmail.com>
2020-02-25 08:52:36 +0000

    added routing

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 
+import { Route, Switch } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <Home></Home>
-      <Rooms></Rooms>
-      <SingleRoom></SingleRoom>
-      <Error></Error>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:single" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
