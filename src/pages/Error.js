f74ff187e7c736466416eb524ccfe9b1b1e6d7fd
Andy Gummer <gummer.andy@gmail.com>
2020-02-26 07:59:24 +0000

    added background image through Hero component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,6 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 export default function Error() {
-  return <div>Hello from error page</div>;
+  return <Hero />;
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 const Home = () => {
-  return <div>Hello from home page</div>;
+  return <Hero />;
 };
 
 export default Home;
